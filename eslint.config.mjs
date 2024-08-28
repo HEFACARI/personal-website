b26a8f25bb4576ecec0d9a1efb5bb187476b158d
HEFACARI <hectorcaez24@gmail.com>
2024-08-28 17:48:29 -0500

    Realiza diferentes cambios grandes en el proyecto

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,13 @@ import pluginReact from "eslint-plugin-react";
 
 
 export default [
+
+  {rules: {
+    'no-console': 'warn', // Permite console.log pero advierte sobre su uso
+    'no-unused-vars': 'warn', // Advierte sobre variables no utilizadas
+    'semi': ['error', 'always'], // Asegura que siempre se utilicen puntos y comas (como error)
+    'quotes': ['error', 'single'], // Asegura que se usen comillas simples (como error)
+  }},
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
